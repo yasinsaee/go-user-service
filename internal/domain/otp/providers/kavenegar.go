820aee7f3b8356c252f043b773fa7a96c56289d1
YasinSaee <yasinvsaee@gmail.com>
2025-11-20 01:20:20 +0330

    add kavenegar service

---
+++ internal/domain/otp/providers/kavenegar.go
@@ -0,0 +1,44 @@
+package providers
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+type SMSProviderKavenegar struct {
+	APIKey     string
+	Sender     string
+	HTTPClient *http.Client
+}
+
+func NewSMSProviderKavenegar(apiKey, sender string) *SMSProviderKavenegar {
+	return &SMSProviderKavenegar{
+		APIKey:     apiKey,
+		Sender:     sender,
+		HTTPClient: http.DefaultClient,
+	}
+}
+
+func (s *SMSProviderKavenegar) Send(receiver string, code string) error {
+	message := fmt.Sprintf("Your OTP code is: %s", code)
+
+	apiURL := fmt.Sprintf("https://api.kavenegar.com/v1/%s/sms/send.json", s.APIKey)
+
+	data := url.Values{}
+	data.Set("receptor", receiver)
+	data.Set("message", message)
+	data.Set("sender", s.Sender)
+
+	resp, err := s.HTTPClient.PostForm(apiURL, data)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		return fmt.Errorf("sms sending failed: status %d", resp.StatusCode)
+	}
+
+	return nil
+}
--- internal/otp/otp.go
+++
@@ -1,5 +0,0 @@
-package otp
-
-type OTPService interface {
-	SendOTP(to string, code string) error
-}
