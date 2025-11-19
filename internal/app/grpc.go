ff7a1656254550fb558660a15138b37a590a1ff1
YasinSaee <yasinvsaee@gmail.com>
2025-11-20 01:31:29 +0330

    add providers

--- internal/app/grpc.go
+++ internal/app/grpc.go
@@ -5,16 +5,21 @@ import (
 	"net"
 
 	"github.com/yasinsaee/go-user-service/internal/app/config"
+	"github.com/yasinsaee/go-user-service/internal/domain/otp/providers"
+	otpgrpc "github.com/yasinsaee/go-user-service/internal/handlers/grpc/otp"
 	permissiongrpc "github.com/yasinsaee/go-user-service/internal/handlers/grpc/permission"
 	rolegrpc "github.com/yasinsaee/go-user-service/internal/handlers/grpc/role"
 	usergrpc "github.com/yasinsaee/go-user-service/internal/handlers/grpc/user"
+	repository_otp "github.com/yasinsaee/go-user-service/internal/repository/otp"
 	repository_permission "github.com/yasinsaee/go-user-service/internal/repository/permission"
 	repository_role "github.com/yasinsaee/go-user-service/internal/repository/role"
 	repository_user "github.com/yasinsaee/go-user-service/internal/repository/user"
+	"github.com/yasinsaee/go-user-service/internal/service/otp"
 	"github.com/yasinsaee/go-user-service/internal/service/permission"
 	"github.com/yasinsaee/go-user-service/internal/service/role"
 	"github.com/yasinsaee/go-user-service/internal/service/user"
 	"github.com/yasinsaee/go-user-service/pkg/mongo"
+	otppb "github.com/yasinsaee/go-user-service/user-service/otp"
 	permissionpb "github.com/yasinsaee/go-user-service/user-service/permission"
 	rolepb "github.com/yasinsaee/go-user-service/user-service/role"
 	userpb "github.com/yasinsaee/go-user-service/user-service/user"
@@ -35,21 +40,28 @@ func StartGRPCServer() {
 	permissionRepo := repository_permission.NewMongoPermissionRepository(mongo.DB.Database, "permission")
 	roleRepo := repository_role.NewMongoRoleRepository(mongo.DB.Database, "role")
 	userRepo := repository_user.NewMongoUserRepository(mongo.DB.Database, "user")
+	otpRepo := repository_otp.NewMongoOTPRepository(mongo.DB.Database, "otp")
+
+	//providers
+	provider := providers.NewOTPProvider()
 
 	//services
 	permissionService := permission.NewPermissionService(permissionRepo)
 	roleService := role.NewRoleService(roleRepo)
 	userService := user.NewUserService(userRepo)
+	otpService := otp.NewOTPService(otpRepo, provider, nil, 120, 60)
 
 	//handlers
 	permissionHandler := permissiongrpc.New(permissionService)
 	roleHandler := rolegrpc.New(roleService, permissionService)
 	userHandler := usergrpc.New(userService, roleService, permissionService)
+	otpHandler := otpgrpc.New(otpService)
 
 	//register grpc services
 	permissionpb.RegisterPermissionServiceServer(s, permissionHandler)
 	rolepb.RegisterRoleServiceServer(s, roleHandler)
 	userpb.RegisterUserServiceServer(s, userHandler)
+	otppb.RegisterOTPServiceServer(s, otpHandler)
 
 	log.Println("gRPC server is running on port 50051")
 	if err := s.Serve(lis); err != nil {
---
+++ internal/domain/otp/providers/providers.go
@@ -0,0 +1,30 @@
+package providers
+
+import (
+	"fmt"
+
+	"github.com/yasinsaee/go-user-service/internal/app/config"
+	"github.com/yasinsaee/go-user-service/internal/domain/otp"
+)
+
+func NewOTPProvider() otp.OTPProvider {
+	providerType := config.GetEnv("OTP_PROVIDER", "KAVENEGAR")
+
+	switch providerType {
+	case "KAVENEGAR":
+		apiKey := config.GetEnv("KAVENEGAR_API_KEY", "")
+		sender := config.GetEnv("KAVENEGAR_SENDER", "")
+		return NewSMSProviderKavenegar(apiKey, sender)
+
+	case "SMTP":
+		// host := config.GetEnv("SMTP_HOST", "")
+		// port := config.GetEnv("SMTP_PORT", "587")
+		// user := config.GetEnv("SMTP_USER", "")
+		// pass := config.GetEnv("SMTP_PASS", "")
+		// return NewEmailProvider(host, port, user, pass)
+
+	default:
+		panic(fmt.Sprintf("unknown OTP provider: %s", providerType))
+	}
+	return nil
+}
